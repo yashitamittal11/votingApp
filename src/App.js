3946f6934ccc476e92ff268f74dc298b6b416233
yashita.mittal <yashita.mittal@a8c.com>
2023-04-25 11:55:42 +0530

    Fixed the README

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ActivitiesList from './components/ActivitiesList';
+import UserPreference from './components/UserPreference';
+import AddActivityForm from './components/AddActivityForm';
+import TopActivities from './components/TopActivities';
+import ResetPreferences from './components/ResetPreferences';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ActivitiesList />
+        <UserPreference />
+        <ResetPreferences />
+        <AddActivityForm />
+        <TopActivities />
     </div>
   );
 }
